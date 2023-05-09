d88c6015eb2868c6f8a6a76246db2c8a7a3194ac
evertonpsantos <evertonpsantos8@gmail.com>
2023-05-09 16:25:29 -0300

    chore: changing Queue data name

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -4,23 +4,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.__data = deque()
+        self.data = deque()
 
     def __len__(self):
-        return len(self.__data)
+        return len(self.data)
 
     def is_empty(self):
-        return not len(self.__data)
+        return not len(self.data)
 
     def enqueue(self, value):
-        self.__data.append(value)
+        self.data.append(value)
 
     def dequeue(self):
         if self.is_empty():
             return None
-        return self.__data.popleft()
+        return self.data.popleft()
 
     def search(self, index):
         if index < 0 or index > len(self) - 1:
             raise IndexError("Índice Inválido ou Inexistente")
-        return self.__data[index]
+        return self.data[index]
