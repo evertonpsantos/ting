7197a970e4cf5a9d99d137d16eeef7b2488c3d8b
evertonpsantos <evertonpsantos8@gmail.com>
2023-05-09 20:03:13 -0300

    feat: adds search_by_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,8 +2,8 @@ from ting_file_management.queue import Queue
 
 
 def exists_word(word, instance: Queue):
-    for file in instance.data:
-        found_results = [{
+    found_results = [
+        {
             "arquivo": file["nome_do_arquivo"],
             "palavra": word,
             "ocorrencias": [
@@ -11,12 +11,29 @@ def exists_word(word, instance: Queue):
                 for index, line in enumerate(file["linhas_do_arquivo"])
                 if word.lower() in line.lower()
             ],
-        }]
+        }
+        for file in instance.data
+    ]
 
-        if len(found_results[0]['ocorrencias']) == 0:
-            return []
-        return found_results
+    if len(found_results[0]["ocorrencias"]) == 0:
+        return []
+    return found_results
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação."""
+def search_by_word(word, instance: Queue):
+    found_results = [
+        {
+            "arquivo": file["nome_do_arquivo"],
+            "palavra": word,
+            "ocorrencias": [
+                {"linha": index + 1, 'conteudo': line}
+                for index, line in enumerate(file["linhas_do_arquivo"])
+                if word.lower() in line.lower()
+            ],
+        }
+        for file in instance.data
+    ]
+
+    if len(found_results[0]["ocorrencias"]) == 0:
+        return []
+    return found_results
