e5910af5c7feff9da3089fd7473677d9cec1953b
evertonpsantos <evertonpsantos8@gmail.com>
2023-05-09 19:54:17 -0300

    feat: add exists_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    for file in instance.data:
+        found_results = [{
+            "arquivo": file["nome_do_arquivo"],
+            "palavra": word,
+            "ocorrencias": [
+                {"linha": index + 1}
+                for index, line in enumerate(file["linhas_do_arquivo"])
+                if word.lower() in line.lower()
+            ],
+        }]
+
+        if len(found_results[0]['ocorrencias']) == 0:
+            return []
+        return found_results
 
 
 def search_by_word(word, instance):
