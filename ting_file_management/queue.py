8097617511d64691c6e11ba38485dca8eb9e2146
evertonpsantos <evertonpsantos8@gmail.com>
2023-05-09 14:00:11 -0300

    feat: adds class Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,26 @@
+from collections import deque
 from ting_file_management.abstract_queue import AbstractQueue
 
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.__data = deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.__data)
+
+    def is_empty(self):
+        return not len(self.__data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.__data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.is_empty():
+            return None
+        return self.__data.popleft()
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > len(self) - 1:
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.__data[index]
